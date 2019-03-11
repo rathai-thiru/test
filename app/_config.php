df53b61fb1263760a36d312975c1b2d1e26af0d5
Rathai Thiru <rathai@Rathais-MacBook-Pro.local>
2019-03-12 09:39:22 +1300

    time zone setup

--- app/_config.php
+++ app/_config.php
@@ -7,3 +7,4 @@ use SilverStripe\Security\Member;
 $validator = PasswordValidator::create();
 // Settings are registered via Injector configuration - see passwords.yml in framework
 Member::set_password_validator($validator);
+ini_set('date.timezone', 'Asia/Singapore');
